7184379e247659c0a62d879c6de43a5d42d57439
vswnz <vsw@whs.school.nz>
2022-05-17 14:59:57 +1200

    prints out hte board

---
+++ heftyStuff/lines.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a really complex stuff
+ * By Bill
+ * To show multiple merges
+ */
+public class lines
+{
+    final int COL=5;
+    final int ROW=4;
+    int[][] board = new int[ROW][COL];
+    public lines()
+    {
+        // show board
+        for (int y=0;y<ROW;y++){
+            for (int x=0;x<COL;x++)
+                System.out.print(board[x][y]);
+            System.out.println();
+        }
+    
+
+    }
+
+}
